350f71341cf7ff231628e992b525fd26fa7938fe
“Daffa <mdaffakha@gmail.com>
2025-05-26 18:06:27 +0700

    add setup on project

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <main>
+      <div className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+        <div className="max-w-7xl w-full">
+          <h1 className="bg-red-600">Hello World</h1>
+        </div>
+      </div>
+    </main>
+  );
+}
